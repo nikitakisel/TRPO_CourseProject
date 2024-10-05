d019d0e2f6505a44ea9a48fadfc6f5519d7657ca
Никита Киселев <nikitakiselev@MacBook-Air-Nikita.local>
2024-10-05 12:14:17 +0300

    creating first test

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,2 +1,36 @@
-def test_authorization():
-    assert True
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import main
+
+
+def test_count_activities():
+    main.cursor.execute('''
+    SELECT Count(*) FROM Students WHERE social_activities = 1
+    ''')
+    activities_count = main.cursor.fetchall()[0][0]
+    main.connection.commit()
+    assert activities_count
+
+
+def test_inverse_best():
+    main.cursor.execute('''
+    SELECT Count(*) FROM Students WHERE social_activities = 1
+    ''')
+    count_begin = main.cursor.fetchall()[0][0]
+    main.connection.commit()
+
+    main.cursor.execute('''
+    UPDATE Students
+    SET social_activities = NOT social_activities
+    WHERE marks = '55555'
+    ''')
+    main.connection.commit()
+
+    main.cursor.execute('''
+    SELECT Count(*) FROM Students WHERE social_activities = 1
+    ''')
+    count_end = main.cursor.fetchall()[0][0]
+    main.connection.commit()
+
+    assert abs(count_end - count_begin)
